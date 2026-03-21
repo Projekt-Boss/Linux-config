4927cffd94d0cdebf96654f6eb8c21be00d851bd
Projekt-Boss <schueler.jkremer@tbk-solingen.de>
2026-03-21 08:18:44 +0100

    backup config for all my files to copy paste in the git repo

---
+++ gitBackupConfig.sh
@@ -0,0 +1,5 @@
+cp -r ~/.config/alacritty ~/Documents/gitRepo
+cp -r ~/.config/waybar ~/Documents/gitRepo
+cp -r ~/.config/wofi ~/Documents/gitRepo
+cp -r ~/.config/sway/ ~/Documents/gitRepo
+cp -r ~/.bashrc.d/aliases ~/Documents/gitRepo/bashrc.d
